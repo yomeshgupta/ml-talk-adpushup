539bdd5e6b7af10f108ce6231297ad73f681cdcf
yomeshgupta <yomesh.gupta@gmail.com>
2017-08-18 01:22:01 +0530

    Added empty functions

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def computeCost(X, y, theta):
+	pass
+
+def gradientDescent(X, y, theta, alpha, num_iters):
+	pass
+
+def featureNormalize(X, n):
+	pass
+
 def run():
 	print("Hello World")
 
